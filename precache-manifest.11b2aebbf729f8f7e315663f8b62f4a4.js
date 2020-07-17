b5031d13c277ead3ce5071958350ca8d4ebfc9b0
Afif Al Mamun <afifanik@gmail.com>
2020-07-18 05:41:13 +0600

    Updates

---
+++ precache-manifest.11b2aebbf729f8f7e315663f8b62f4a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc56d238fa10fe14c182027c841c319",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "69732d5547db55caba7c",
+    "url": "/weather-app-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f845c34bc1ed75903621",
+    "url": "/weather-app-react/static/css/main.63fc3a0e.chunk.css"
+  },
+  {
+    "revision": "69732d5547db55caba7c",
+    "url": "/weather-app-react/static/js/2.5ae6ef59.chunk.js"
+  },
+  {
+    "revision": "0889a3293e5e5728b32859a8dd009d0e",
+    "url": "/weather-app-react/static/js/2.5ae6ef59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f845c34bc1ed75903621",
+    "url": "/weather-app-react/static/js/main.083c5136.chunk.js"
+  },
+  {
+    "revision": "73ec9f2eed65a5d44827",
+    "url": "/weather-app-react/static/js/runtime-main.4ee40ab0.js"
+  }
+]);
